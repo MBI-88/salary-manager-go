a0f8ba5a6fe138906f9c7d25822daa01b284402f
MBI-88 <ingmbi8807@gmail.com>
2026-02-14 19:00:04 -0300

    feature: sync

--- tests/salary_manager_test.go
+++ tests/salary_manager_test.go
@@ -7,22 +7,22 @@ import (
 
 func TestProcessData(t *testing.T) {
 	acmTest := sm.NewSalaryManager()
-	test := make(map[string]int)
+	test := make(map[string]float64)
 	sample := []string{
 		"JOSE=MO08:00-19:00",
 		"JUAN=TH12:00-17:00,FR09:01-16:00",
 		"ANA=SA09:00-13:00,MO08:10-16:00",
 	}
 
-	test["JOSE"] = 179 // 25 + 135 + 20
-	test["JUAN"] = 179 // 75 + 105 = 180
-	test["ANA"] = 209 // 60 + 105
+	test["JOSE"] = 179.99 // 25 + 135 + 20
+	test["JUAN"] = 179.99 // 75 + 105 = 180
+	test["ANA"] = 209.92 // 60 + 105
 
 	restul := acmTest.ProcessData(sample)
 
 	for key := range restul {
-		if int(restul[key]) != test[key] {
-			t.Errorf("Name %s resutl => %d and test => %d\n", key, int(restul[key]), test[key])
+		if int(restul[key]) != int(test[key]) {
+			t.Errorf("Name %s resutl => %.2f and test => %.2f\n", key, restul[key], test[key])
 		}
 	}
 
